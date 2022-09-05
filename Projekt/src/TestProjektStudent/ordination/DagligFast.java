061ebd1ae735b2766d3b06be2bb3e43ea10758a4
Rosell22V <Gustav@klitholm.com>
2022-09-05 11:14:18 +0200

    test

--- Projekt/src/TestProjektStudent/ordination/DagligFast.java
+++ Projekt/src/TestProjektStudent/ordination/DagligFast.java
@@ -17,21 +17,8 @@ public class DagligFast extends Ordination {
         return dosis;
     }
 
-    private LocalDate fra;
-    private LocalDate til;
-    private double morgen;
-    private double middag;
-    private double aften;
-    private double nat;
-
-    public DagligFast(LocalDate fra, LocalDate til, double morgen, double middag, double aften, double nat){
-        super();
-        this.fra = fra;
-        this.til = til;
-        this.morgen = morgen;
-        this.middag = middag;
-        this.aften = aften;
-        this.nat = nat;
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Patient patient){
+        super(startDen, slutDen, patient);
     }
 
     @Override
