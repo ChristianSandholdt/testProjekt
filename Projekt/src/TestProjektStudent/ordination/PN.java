8e95232bdbc3360e4de43fa2d444a0e849626b9b
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-09-05 11:04:47 +0200

    Patient --> Ordination

--- Projekt/src/TestProjektStudent/ordination/PN.java
+++ Projekt/src/TestProjektStudent/ordination/PN.java
@@ -1,9 +1,16 @@
 package TestProjektStudent.ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class PN extends Ordination {
     private double antalEnheder;
+
+    private double antalGange = 0;
+
+    public PN(LocalDate startDen, LocalDate slutDen, Patient patient) {
+        super(startDen, slutDen, patient);
+    }
 
     /**
      * Registrer, at der er givet en dosis paa dagen givesDen.
@@ -11,13 +18,16 @@ public class PN extends Ordination {
      * Returner false ellers, og datoen givesDen ignoreres.
      */
     public boolean givDosis(LocalDate givesDen) {
-
+        ArrayList<LocalDate> datoer = new ArrayList<>();
+        if (givesDen.isAfter(getStartDen()) && givesDen.isBefore(getSlutDen())){
+            datoer.add(givesDen);
+            return true;
+        }
         return false;
     }
 
     /** Returner antal gange ordinationen er anvendt. */
     public int getAntalGangeGivet() {
-
         return -1;
     }
 
